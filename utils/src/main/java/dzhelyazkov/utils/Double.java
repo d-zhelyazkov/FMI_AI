c263702d722f8c5fbe216451ff1f7b20b73c24b9
Danislav Zhelyazkov <dzhelyazkov@axway.com>
2017-11-17 16:02:33 +0200

    Utils: Added link for Double.compare.

--- utils/src/main/java/dzhelyazkov/utils/Double.java
+++ utils/src/main/java/dzhelyazkov/utils/Double.java
@@ -4,6 +4,9 @@ public class Double {
 
     private static final double EPSILON = 5.96e-08;
 
+    /**
+     * https://www.ibm.com/developerworks/java/library/j-jtp0114/#N10255
+     */
     public static int compare(double d1, double d2) {
         return (java.lang.Math.abs(d1 / d2 - 1) < EPSILON) ? 0 : java.lang.Double.compare(d1, d2);
     }
