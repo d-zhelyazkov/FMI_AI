4e467ba5c99bd9f9e3bb0b691f862596548683a3
Danislav Zhelyazkov <dzhelyazkov@axway.com>
2017-12-03 18:26:55 +0200

    Utils: performance improvement.

--- utils/src/main/java/dzhelyazkov/utils/Collections.java
+++ utils/src/main/java/dzhelyazkov/utils/Collections.java
@@ -1,14 +1,20 @@
 package dzhelyazkov.utils;
 
-import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
 public class Collections {
 
     public static <T> T getRandomElement(Collection<T> elements) {
-        return getRandomElement(new ArrayList<>(elements));
+        int elemIx = new Random().nextInt(elements.size());
+        Iterator<T> iterator = elements.iterator();
+        while ((elemIx--) > 0) {
+            iterator.next();
+        }
+
+        return iterator.next();
     }
 
     public static <T> T getRandomElement(List<T> elements) {
